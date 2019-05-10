a32106427ef715aed67680726de7981e5c5bbe8f
unknown <xu_junkai@foxmail.com>
2019-05-10 16:19:36 +0800

    123

--- roles/erpnext/files/init.sh
+++ roles/erpnext/files/init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 获取mysql 默认密码
-old_password=$(cat /root/password.txt | awk -F ":" '{print $2}' )
+old_password=$(cat /credentials/password.txt | awk -F ":" '{print $2}' )
 
  # 截取随机生成的用户名
 nexterp_user=$(cat /home/frapper/frappe-bench/sites/site1.local/site_config.json | awk 'NR'==2 | awk -F "\"" '{print $4}')
@@ -23,7 +23,7 @@ mysqladmin -uroot -p${old_password} password $new_password
 
 mysql -uroot -p${new_password} -e "use mysql;UPDATE mysql.user SET password = PASSWORD('$new_erpnext_pwd') WHERE USER = '$nexterp_user';FLUSH PRIVILEGES;" # 修改erpnext密码
 
-echo 'Databases root Password:'$new_password  > /root/password.txt
+echo 'Databases root Password:'$new_password  > /credentials/password.txt
 
 sed -i "s/$old_nexterp_pwd/$new_erpnext_pwd/" /home/frapper/frappe-bench/sites/site1.local/site_config.json
 
