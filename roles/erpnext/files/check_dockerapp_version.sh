1d4e7fefca4417563b54f9ff82e04d497185fbe7
zengxc <zxc916684@163.com>
2020-11-04 14:35:04 +0800

    main

--- roles/erpnext/files/check_dockerapp_version.sh
+++ roles/erpnext/files/check_dockerapp_version.sh
@@ -1,4 +1,3 @@
- #!/bin/bash
  sudo echo "redis_version:" $(docker exec -it erpnext_redis-queue_1 /bin/bash -c 'redis-server -v |cut -d " " -f 3') |sudo tee -a /data/logs/install_version.txt  
 
  sudo echo $(docker exec -it   erpnext_erpnext-nginx_1 /bin/bash -c "nginx -v") |sudo tee -a /data/logs/install_version.txt
