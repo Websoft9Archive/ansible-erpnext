9f1f94f408a040d005aeb8f26bbd02e926f94f5a
zengxc <zxc916684@163.com>
2020-11-04 14:33:52 +0800

    main

--- roles/erpnext/files/check_dockerapp_version.sh
+++ roles/erpnext/files/check_dockerapp_version.sh
@@ -3,6 +3,6 @@
 
  sudo echo $(docker exec -it   erpnext_erpnext-nginx_1 /bin/bash -c "nginx -v") |sudo tee -a /data/logs/install_version.txt
 
- sudo echo $(docker exec -it erpnext_erpnext-worker-default_1 /bin/bash -c "cat app_versions.json |sed -n 2p") |sudo tee -a /data/logs/install_version.txt
- 
+ sudo echo $(docker exec -it erpnext_erpnext-worker-default_1 /bin/bash -c "cat app_versions.json |sed -n 2p |sed 's/\"//g'") |sudo tee -a /data/logs/install_version.txt
+
  rm $0 
