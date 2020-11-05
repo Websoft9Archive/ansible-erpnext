ac6b0a086a834228e838dbac4a978749b69c23d3
zengxc <zxc916684@163.com>
2020-11-05 15:23:14 +0800

    main

--- roles/erpnext/files/check_dockerapp_version.sh
+++ roles/erpnext/files/check_dockerapp_version.sh
@@ -1,8 +1,8 @@
  #!/bin/bash
- sudo sh -c "echo "redis_version:" "docker exec -it erpnext_redis-queue_1 /bin/bash -c 'redis-server -v |cut -d " " -f 3'" &>> /data/logs/install_version.txt"  
+ sudo "echo "redis_version: `docker exec -it erpnext_redis-queue_1 /bin/bash -c 'redis-server -v |cut -d " " -f 3'`" |tee -a /data/logs/install_version.txt  
 
- sudo sh -c "echo "docker exec -it erpnext_erpnext-nginx_1 /bin/bash -c "nginx -v"" &>> /data/logs/install_version.txt"
+ sudo "echo `docker exec -it erpnext_erpnext-nginx_1 /bin/bash -c "nginx -v"`" |tee -a /data/logs/install_version.txt
 
- sudo sh -c "echo "docker exec -it erpnext_erpnext-worker-default_1 /bin/bash -c "cat app_versions.json |sed -n 2p |sed 's/\"//g'"" &>> /data/logs/install_version.txt"
+ sudo "echo `docker exec -it erpnext_erpnext-worker-default_1 /bin/bash -c "cat app_versions.json |sed -n 2p |sed 's/\"//g'`" |tee -a /data/logs/install_version.txt
              
  rm $0 
